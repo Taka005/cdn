e60e7ca9fb3fc2cb8d657d83052d599a24d6de27
Taka005 <01taka005@gmail.com>
2022-10-17 18:03:31 +0900

    修正

--- js/translate.js
+++ js/translate.js
@@ -1,16 +1,18 @@
 class translate{
-    async get(text,lang){
-        if(text.length > 5000){
-            throw new Error("The maximum number of translations is 5000 characters or less");
-        }
-        const res_text = await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&dj=1&q=${encodeURIComponent(text)}`)
-            .then(res => res.json())
-            .catch(error =>{
-                throw new Error(error)
-            });
-
-        return res_text.sentences.map((sentence)=>{
-            return sentence.trans
-        }).join("");
+    get(text,lang){
+        return new Promise((resolve, reject) => {
+            if(text.length > 5000){
+                throw new Error("The maximum number of translations is 5000 characters or less");
+            }
+            fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&dj=1&q=${encodeURIComponent(text)}`)
+                .then(res => res.json())
+                .then(res =>{
+                    res.sentences.map((sentence)=>{
+                        return sentence.trans
+                    }).join("");
+                })
+                .then(resolve)
+                .catch(reject);
+        })
     }
 }
